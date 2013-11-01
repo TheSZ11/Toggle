719a28327ce56e718de14bd9e557f37adbcb08ee
Sajjad Zaidi <sajjadzaidi11@gmail.com>
2013-10-31 22:27:46 -0400

    added functionality for widget

--- gen/com/szaidi/silentmodetoggle/R.java
+++ gen/com/szaidi/silentmodetoggle/R.java
@@ -17,8 +17,8 @@ public final class R {
          screen margins) for sw720dp devices (e.g. 10" tablets) in landscape here.
     
          */
-        public static final int activity_horizontal_margin=0x7f040000;
-        public static final int activity_vertical_margin=0x7f040001;
+        public static final int activity_horizontal_margin=0x7f050000;
+        public static final int activity_vertical_margin=0x7f050001;
     }
     public static final class drawable {
         public static final int ic_launcher=0x7f020000;
@@ -26,21 +26,23 @@ public final class R {
         public static final int phone_on=0x7f020002;
     }
     public static final class id {
-        public static final int action_settings=0x7f080002;
-        public static final int phone_icon=0x7f080000;
-        public static final int toggleButton=0x7f080001;
+        public static final int action_settings=0x7f090003;
+        public static final int phoneState=0x7f090002;
+        public static final int phone_icon=0x7f090000;
+        public static final int toggleButton=0x7f090001;
     }
     public static final class layout {
         public static final int activity_main=0x7f030000;
+        public static final int widget=0x7f030001;
     }
     public static final class menu {
-        public static final int main=0x7f070000;
+        public static final int main=0x7f080000;
     }
     public static final class string {
-        public static final int action_settings=0x7f050001;
-        public static final int app_name=0x7f050000;
-        public static final int hello_world=0x7f050002;
-        public static final int toggle_silent_mode=0x7f050003;
+        public static final int action_settings=0x7f060001;
+        public static final int app_name=0x7f060000;
+        public static final int hello_world=0x7f060002;
+        public static final int toggle_silent_mode=0x7f060003;
     }
     public static final class style {
         /** 
@@ -64,10 +66,13 @@ public final class R {
     
  API 14 theme customizations can go here. 
          */
-        public static final int AppBaseTheme=0x7f060000;
+        public static final int AppBaseTheme=0x7f070000;
         /**  Application theme. 
  All customizations that are NOT specific to a particular API-level can go here. 
          */
-        public static final int AppTheme=0x7f060001;
+        public static final int AppTheme=0x7f070001;
+    }
+    public static final class xml {
+        public static final int widget_provider=0x7f040000;
     }
 }
